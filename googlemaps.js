4e8a7e250d667435bd6fc4cb5ef1ecbfc9cf1434
Ben Davis <bendavis78@gmail.com>
2016-03-13 01:02:27 -0600

    hack to get google maps working w/ whatever broke in systemjs

--- googlemaps.js
+++ googlemaps.js
@@ -1,20 +1,20 @@
 function injectScript(src){
-  return new Promise(function(resolve, reject) {
-    window["__google_maps_callback__"] = function() {
+  return new Promise(function(resolve) {
+    window.__google_maps_callback__ = function() {
       resolve('');
-    }
+    };
 
     var s = document.createElement('script'); s.type = 'text/javascript'; s.async = true; s.src = src;
     var t = document.getElementsByTagName('script')[0];
     if (t) { t.parentNode.insertBefore(s, t); }
-      else { var h = document.getElementsByTagName("head")[0]; h.appendChild(s); }
+      else { var h = document.getElementsByTagName('head')[0]; h.appendChild(s); }
   });
 }
 
-exports.fetch = function(load) {
-  var scriptUrl = load.address
+exports.fetch = function() {
+  var scriptUrl = 'https://maps.googleapis.com/maps/api/js';
   scriptUrl += (scriptUrl.indexOf('?') < 0)? '?' : '&';
-  scriptUrl += "callback=__google_maps_callback__"
+  scriptUrl += 'callback=__google_maps_callback__';
   return injectScript(scriptUrl).then(function() {
     return 'module.exports = google.maps';
   });
